436d53c82275b9a1a06b74b937d31b5f6ea7a301
Paweł J <jpawz@outlook.com>
2023-03-12 20:49:07 +0100

    Add fetchWeatherForLocation function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Weather App works!");
+const apiKey = "f29a957b5cdc26753a48e3fe93196b8f";
+async function fetchWeatherForLocation(latitude, longitude) {
+  const openWeatherMapApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  const response = await fetch(openWeatherMapApi, { mode: "cors" });
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+
+fetchWeatherForLocation(50.0334592, 21.9840512);
