63cbb1c33d66438533af61b74d14064c6d005c92
Paweł J <jpawz@outlook.com>
2023-03-14 20:48:15 +0100

    Display weather on page

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -10,7 +10,22 @@ export default class UI {
     button.addEventListener("click", () => {
       const latitude = document.getElementById("latitude").value;
       const longitude = document.getElementById("longitude").value;
-      Weather.fetchWeatherForLocation(latitude, longitude).then(console.log);
+      Weather.fetchWeatherForLocation(latitude, longitude).then(
+        UI.updateWeatherData
+      );
     });
+  }
+
+  static updateWeatherData(weatherDto) {
+    document.getElementById("temperature").innerText = weatherDto.temperature;
+    document.getElementById("feelsLikeTemperature").innerText =
+      weatherDto.feelsLikeTemperature;
+    document.getElementById("humidity").innerText = weatherDto.humidity;
+    document.getElementById("pressure").innerText = weatherDto.pressure;
+    document.getElementById("visibility").innerText = weatherDto.visibility;
+    document.getElementById("windSpeed").innerText = weatherDto.windSpeed;
+    document.getElementById("description").innerText =
+      weatherDto.weatherDescription;
+    document.getElementById("weatherState").innerText = weatherDto.weatherState;
   }
 }
