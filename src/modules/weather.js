40daa9e6b5f2cb55e091160503187f1b52c22259
Paweł J <jpawz@outlook.com>
2023-03-19 22:15:52 +0100

    Change units to metric

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -4,7 +4,7 @@ export default class Weather {
   static apiKey = "f29a957b5cdc26753a48e3fe93196b8f";
 
   static async fetchWeatherForLocation(latitude, longitude) {
-    const openWeatherMapApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${Weather.apiKey}`;
+    const openWeatherMapApi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${Weather.apiKey}&units=metric`;
     const response = await fetch(openWeatherMapApi, { mode: "cors" });
     const weatherData = await response.json();
     console.log(weatherData);
